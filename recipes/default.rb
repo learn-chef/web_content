152f2e45595f145305e9fc94f030f86c6012d89c
tpetchel <tpetchel@gmail.com>
2017-03-01 09:08:58 -0500

    Initial cookbook

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,5 @@
-#
-# Cookbook:: web_content
-# Recipe:: default
-#
-# Copyright:: 2017, The Authors, All Rights Reserved.
+directory node['web_content']['document_root'] do
+  rights :read, node['web_content']['group']
+  recursive true
+  action :create
+end
